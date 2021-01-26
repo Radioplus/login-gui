feeadb00b9d5ec3fb4dd26fefd31796568e72277
Radio <72998522+Radioplus@users.noreply.github.com>
2021-01-26 22:13:52 +0300

    Add files via upload

---
+++ Radio's Random Code/src/LoginSystem.java
@@ -0,0 +1,135 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        IDandPasswords idandPasswords = new IDandPasswords();
+
+        LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());
+
+    }
+}
+
+class IDandPasswords {
+
+    HashMap<String,String> logininfo = new HashMap<String,String>();
+
+    IDandPasswords(){
+
+        logininfo.put("Test1","password1");
+        logininfo.put("Test2","password2");
+        logininfo.put("Test3","password3");
+        logininfo.put("Test4","password4");
+        logininfo.put("Test5","password5");
+
+    }
+
+    public HashMap<String, String> getLoginInfo(){
+        return logininfo;
+    }
+}
+
+class LoginPage implements ActionListener {
+
+    JFrame frame = new JFrame();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Restart");
+    JTextField userIDField = new JTextField();
+    JPasswordField userPasswordField = new JPasswordField();
+    JLabel userIDLabel = new JLabel("Username:");
+    JLabel userPasswordLabel = new JLabel("Password:");
+    JLabel messageLabel = new JLabel();
+    HashMap<String,String> logininfo = new HashMap<String,String>();
+
+    LoginPage(HashMap<String,String> loginInfoOriginal){
+
+        logininfo = loginInfoOriginal;
+
+        userIDLabel.setBounds(50,100,75,25);
+        userPasswordLabel.setBounds(50,150,75,25);
+
+        messageLabel.setBounds(125,250,250,35);
+        messageLabel.setFont(new Font(null, Font.ITALIC,25));
+
+        userIDField.setBounds(125,100,200,25);
+        userPasswordField.setBounds(125,150,200,25);
+
+        loginButton.setBounds(125,200,100,25);
+        loginButton.setFocusable(false);
+        loginButton.addActionListener(this);
+
+        resetButton.setBounds(225,200,100,25);
+        resetButton.setFocusable(false);
+        resetButton.addActionListener(this);
+
+        frame.add(userIDLabel);
+        frame.add(userPasswordLabel);
+        frame.add(messageLabel);
+        frame.add(userIDField);
+        frame.add(userPasswordField);
+        frame.add(loginButton);
+        frame.add(resetButton);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(450,350);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource()==resetButton) {
+            userIDField.setText("");
+            userPasswordField.setText("");
+        }
+
+        if(e.getSource()==loginButton) {
+
+            String userID = userIDField.getText();
+            String password = String.valueOf(userPasswordField.getPassword());
+
+            if(logininfo.containsKey(userID)) {
+                if(logininfo.get(userID).equals(password)) {
+                    messageLabel.setForeground(Color.green);
+                    messageLabel.setText("Login successful");
+                    frame.dispose();
+                    WelcomePage welcomePage = new WelcomePage(userID);
+                }
+                else {
+                    messageLabel.setForeground(Color.red);
+                    messageLabel.setText("Wrong password");
+                }
+
+            }
+            else {
+                messageLabel.setForeground(Color.red);
+                messageLabel.setText("Username not found");
+            }
+        }
+    }
+}
+
+class WelcomePage {
+
+    JFrame frame = new JFrame();
+    JLabel welcomeLabel = new JLabel("Hello!");
+
+    WelcomePage(String userID){
+
+        welcomeLabel.setBounds(0,0,200,35);
+        welcomeLabel.setFont(new Font(null,Font.PLAIN,25));
+        welcomeLabel.setText("Welcome! "+userID);
+
+        frame.add(welcomeLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 350);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
